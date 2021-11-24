26447fde488b79431ffc09ea0695b426b4c66160
Rickson <rickson.simoes@hotmail.com>
2021-11-24 20:56:39 -0300

    :package: puppeteer, handlebars,chrome-aws-lambda,dayjs/ :hammer: Functions verify & generate

---
+++ src/utils/dynamodbClient.ts
@@ -0,0 +1,15 @@
+import { DynamoDB } from "aws-sdk";
+
+const options = {
+  region: "localhost",
+  endpoint: "http://localhost:8000"
+};
+
+
+const isOffline = () => {
+  return false
+}
+
+export const document = isOffline() 
+? new DynamoDB.DocumentClient(options) 
+: new DynamoDB.DocumentClient();
